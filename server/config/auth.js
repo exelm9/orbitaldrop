bacff8907e7ed83742a69802f3e53b3719cee219
Rex Kelly <rexrkelly.on.github@gmail.com>
2016-05-31 12:55:30 -0700

    Update auth.js

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'github' : {
     clientID: '58a7aeed22f6866e9366',
-    clientSecret: '116b354f3c5b782ffc46df15aec8f92a783c9852',
+    clientSecret: 'd9756745164262158f0f16eeea650043633e0127',
     callbackURL: "http://192.81.135.170/login/github/return"
 	}
 }
